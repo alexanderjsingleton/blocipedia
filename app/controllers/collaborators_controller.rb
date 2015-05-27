fc1e3e1eb6892b29df9421136d7d1976b8e68bc6
Alex Singleton <alexanderjsingleton@gmail.com>
2015-05-27 16:50:16 -0500

    configured collaborators

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,13 +1,35 @@
 class CollaboratorsController < ApplicationController
-  def index
+
+  def create
+    @wiki = Wiki.friendly.find(params[:wiki_id])
+    @collaborator = @wiki.collaborators.build(collaborator_params)
+
+    if @collaborator.save
+      flash[:notice] = "Collaborator was saved."
+    else
+      flash[:error] = "There was an error saving collaborator. Please try again."
+    end
+
+    redirect_to edit_wiki_path(@wiki)
   end
 
-  def show
+  def destroy
+    @wiki = Wiki.friendly.find(params[:wiki_id])
+    @collaborator = @wiki.collaborators.find(params[:id])
+
+    if @collaborator.destroy
+      flash[:notice] = "Collaborator was removed from wiki."
+    else
+      flash[:error] = "There was an error removing collaborator. Please try again."
+    end
+
+    redirect_to edit_wiki_path(@wiki)
   end
 
-  def new
-  end
+  
+  private
 
-  def edit
+  def collaborator_params
+    params.require(:collaborator).permit(:user_id, :wiki_id)
   end
 end
